c9856a4c89e458153f73fff2a18e1191c283b1c1
Alexandre <bruneau@diveboard.com>
2017-05-06 17:34:10 +0200

    initial commit

---
+++ app/src/main/java/com/grosalex/androidassistantthings/MainActivity.java
@@ -0,0 +1,17 @@
+package com.grosalex.androidassistantthings;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: Hello World");
+    }
+}
